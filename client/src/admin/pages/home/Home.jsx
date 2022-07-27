403e14d400b299c1a40f80947275965f04f17913
Tanya <tanyadiit7@gmail.com>
2022-07-27 22:20:38 +0530

    changed routing

--- client/src/admin/pages/home/Home.jsx
+++ client/src/admin/pages/home/Home.jsx
@@ -1,10 +1,7 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
-import Widget from "../../components/widget/Widget";
-import Featured from "../../components/featured/Featured";
-import Chart from "../../components/chart/Chart";
-import Table from "../../components/table/Table";
+
 
 const Home = () => {
   return (
@@ -12,20 +9,7 @@ const Home = () => {
       <Sidebar />
       <div className="AdminHomeContainer">
         <Navbar />
-        <div className="widgets">
-          <Widget type="user" />
-          <Widget type="order" />
-          <Widget type="earning" />
-          <Widget type="balance" />
-        </div>
-        <div className="charts">
-          <Featured />
-          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-        </div>
-        <div className="AdminListContainer">
-          <div className="listTitle">Latest Transactions</div>
-          <Table />
-        </div>
+
       </div>
     </div>
   );
--- client/src/main/pages/list/List.jsx
+++ client/src/main/pages/list/List.jsx
@@ -1,5 +1,4 @@
 import "./list.scss"
-import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
 import Datatable from "../../components/datatable/Datatable"
 
