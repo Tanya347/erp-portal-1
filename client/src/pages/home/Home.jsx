70f9309c9c7da611fbcbe3b4e547561d52c1b866
Tanya <tanyadiit7@gmail.com>
2022-08-30 11:21:20 +0530

    Some changes to home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -9,26 +9,43 @@ import { useState } from "react";
 
 const Home = ({ type }) => {
 
+  const slides = document.querySelectorAll('.slide')
+
+  const clearActives = () => {
+    slides.forEach(slide => slide.classList.remove('active'))
+  }
+
+  const setActive = (e) => {
+    clearActives()
+    e.target.classList.add('active')
+  }
+
+  slides.forEach(slide => {
+    slide.addEventListener('click', setActive)
+  })
+
   return (
+
     <div className="home">
       {/* {type === "Admin" && <Sidebar />} */}
       {/* {type === "Admin" && <NavSidebar />} */}
       <div className="AdminHomeContainer">
         {(type === "Admin") ? (<AdminNavbar />) : (<Navbar />)}
-        <div className="widgets">
+        {/* <div className="widgets">
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
-        </div>
-        <div className="charts">
+        </div> */}
+        {/* <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-        </div>
-        <div className="AdminListContainer">
+        </div> */}
+
+        {/* <div className="AdminListContainer">
           <div className="listTitle">Upcoming Events</div>
           <Table type="updates" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
